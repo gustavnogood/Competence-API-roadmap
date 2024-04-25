1215dc1cb77a726c399186a29b28b70939a4b99d
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-04-25 11:44:31 +0200

    added logging

--- RoadmapFunctionApp/Startup.cs
+++ RoadmapFunctionApp/Startup.cs
@@ -1,15 +1,14 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.WebJobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using System.Collections.Generic;
-using Microsoft.Azure.Cosmos;
-using Azure.Identity;
 
 namespace Company.Function
 {
