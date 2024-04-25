8cdd29dd2d6b54c0208a6d8e530b2eb90281f41d
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-04-25 09:08:00 +0200

    added logs

--- RoadmapFunctionApp/Startup.cs
+++ RoadmapFunctionApp/Startup.cs
@@ -20,6 +20,8 @@ namespace Company.Function
     [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "users")] HttpRequest req,
     ILogger log)
         {
+            log.LogInformation("CreateUser function started.");
+
             CosmosClient client = new CosmosClient("https://cosmos-competence-test.documents.azure.com:443/", "r0ppqOeMX7GTifP0vAF4G8w6zFUv5IS74hYqTYJMzGCC2dOb81MYHuwWSnWKsOiadJ7qpXSBZOnIACDbbRybHg==");
 
             Container container = client.GetContainer("competence", "users") ?? throw new NullReferenceException();
@@ -30,10 +32,12 @@ namespace Company.Function
 
             if (!string.IsNullOrEmpty(data.displayName))
             {
+                log.LogInformation($"Attempting to upsert user with display name: {data.displayName}");
                 ItemResponse<UserRequest> request = await container.UpsertItemAsync(data, new PartitionKey(data.displayName));
+                log.LogInformation("User upserted successfully.");
                 return new OkObjectResult(request.Resource);
             }
-
+            log.LogWarning("Failed to upload, no users found in request.");
             return new OkObjectResult("Failed to upload, no users found in request.");
         }
 
