7366b58f7732246d32c72051724587d770c70814
gustavnogood <105938111+gustavnogood@users.noreply.github.com>
2024-05-22 18:32:26 +0200

    changed namespace

--- api/Startup.cs
+++ api/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Company.Function
+namespace Competence.Function
 {
     public class GetFunction
     {
